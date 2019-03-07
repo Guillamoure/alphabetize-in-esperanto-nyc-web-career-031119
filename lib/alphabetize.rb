328ff03656b00991c0c43c24476ad9d3ce5aa462
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-07 18:24:24 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   # code here
   esp = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  new_array = arr.sort_by { |word| word.tr(esp)}
+  new_array = arr.sort_by { |a, b| word.tr(esp)}
   return new_array
 end
