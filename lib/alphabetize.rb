653f4887646ae34d41be2a7e80e7745dbef0b2fe
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-07 18:53:32 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,9 @@
 require 'pry'
 def alphabetize(arr)
   # code here
-  converted = arr[0].tr('abcĉdefgĝhĥijĵklmnoprsŝtuŭvz', 'abcdefghijklmnopqrstuwxyz123')
+  converted = arr[0].tr('abcĉdefgĝhĥijĵklmnoprsŝtuŭvz', 'abcdefghijklmnopqrstuwxyz')
+  alphabet = ["b", "B", "d", "A", "a"]
+  sort_a = alphabet.sort
   binding.pry
   new_array = arr.sort { |word| abcdefghijklmnopqrstuwxyz}
   return new_array
